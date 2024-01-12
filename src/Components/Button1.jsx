a61fc816177a4714fc02023de853539f355ef835
Shubham-391 <subhamy1234@gmail.com>
2024-01-12 15:08:06 +0530

    header

---
+++ src/Components/Button1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Button1(props) {
+  return (
+    <>
+      <button
+        className={`bg-[#BD7D41] py-[19px] px-[48px] text-white text-center font-poppins text-xl not-italic font-semibold leading-normal duration-300 relative group overflow-hidden inline-flex`}
+      >
+        <span className="absolute duration-300 inset-0 bg-sky-400 w-0 group-hover:w-full"></span>
+        <span className="relative">{props.text}</span>
+      </button>
+    </>
+  );
+}
+
+export default Button1
---
+++ src/Components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Button1 from "./Button1";
+
+
+function Header() {
+  return (
+    <>
+      <div className="bg-headerbg bg-cover bg-no-repeat bg-center min-h-screen min-[1200px]:min-h-[932px] min-[1866px]:min-h-screen flex flex-col">
+        <Navbar />
+        <div className="grow flex flex-col items-center px-3">
+          <h1 className="mt-[50px] min-[1200px]:mt-[135px] max-w-[908px] text-white text-center font-poppins text-[30px] md:text-5xl not-italic font-bold leading-normal mb-[15px]">
+            Awesome Design Best Furniture For Your Interior
+          </h1>
+          <p className="text-white text-center font-poppins text-xl not-italic font-normal leading-normal mb-[51px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+          </p>
+          <Button1 text="SHOP NOW" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
